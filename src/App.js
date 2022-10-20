c851db1e54fac7480b41b5a774d981c1a757d933
hirata11 <hirata11@hotmail.com>
2022-10-20 15:10:01 -0300

    9.6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aplicação versão 0.9.5
+          Aplicação versão 0.9.6
         </p>
         <a
           className="App-link"
