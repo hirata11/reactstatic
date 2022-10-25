692b354ab283683153bb6ffd2ac08fdd12ae7082
DevOps <devops@vibesaude.com>
2022-10-25 15:37:24 -0300

    v3.0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aplicação versão 2.9
+          Aplicação versão 3.0
         </p>
         <a
           className="App-link"
